52a971362d1a75d484a876e089bf05549622dc6d
tuenguyen <dactue1006@gmail.com>
2021-04-03 09:42:29 +0700

    Demo Application insights Azure

---
+++ index.js
@@ -0,0 +1,27 @@
+const express = require('express')
+const app = express()
+const port = process.env.PORT || 3000
+
+require('dotenv').config();
+
+let appInsights = require("applicationinsights");
+
+appInsights.setup(process.env.INSTRUMENTATION_KEY)
+    .setAutoDependencyCorrelation(true)
+    .setAutoCollectRequests(true)
+    .setAutoCollectPerformance(true, true)
+    .setAutoCollectExceptions(true)
+    .setAutoCollectDependencies(true)
+    .setAutoCollectConsole(true)
+    .setUseDiskRetryCaching(true)
+    .setSendLiveMetrics(true)
+    .setDistributedTracingMode(appInsights.DistributedTracingModes.AI)
+    .start();
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
